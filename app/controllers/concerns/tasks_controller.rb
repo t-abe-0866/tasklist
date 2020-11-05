3ba7cad2ecc44e2ec5e3ce7c6f8849f62fcac0f9
EC2 Default User <ec2-user@ip-172-31-89-235.ec2.internal>
2020-11-05 05:32:33 +0000

    fix

--- app/controllers/concerns/tasks_controller.rb
+++ app/controllers/concerns/tasks_controller.rb
@@ -1,9 +1,9 @@
 class TasksController < ApplicationController
-      before_action :require_user_logged_in
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = current_user.tasks#Task.all 
+    @tasks = current_user.tasks
   end
 
   def show
@@ -27,6 +27,7 @@ class TasksController < ApplicationController
   end
 
   def edit
+
   end
   
   def update
@@ -55,6 +56,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
